0fa8cab805abb35cf6fdfffd26405ab0bfeeb830
Aayush Singh <97450185+aayushsingh7@users.noreply.github.com>
2026-05-11 11:21:42 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.split(" ").join("").toLowerCase();
+        const n = s.length;
+        let i = 0,
+            j = n - 1;
+        while (i < j) {
+            while (!(("a" <= s[i] && s[i] <= "z") || ("0" <= s[i] && s[i] <= "9")) && i < j) i++;
+            while (!(("a" <= s[j] && s[j] <= "z") || ("0" <= s[j] && s[j] <= "9")) && j > i) j--;
+            if (s[i] != s[j]) return false;
+            j--;
+            i++;
+            console.log(i,j)
+        }
+        return true;
+    }
+}
