44316e2b1a2524042b8530a57b12526924d86c70
Aayush Singh <97450185+aayushsingh7@users.noreply.github.com>
2026-05-30 11:47:53 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.js
@@ -0,0 +1,36 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if (!this.keyStore.has(key)) this.keyStore.set(key, []);
+        this.keyStore.get(key).push([timestamp, value]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if (!this.keyStore.has(key)) return "";
+        let arr = this.keyStore.get(key);
+        let l = 0,
+            r = arr.length - 1;
+        while (l < r) {
+            let m = l + Math.floor((r - l) / 2);
+            if (arr[m][0] >= timestamp) r = m;
+            else l = m + 1;
+        }
+
+        if(arr[l][0] <= timestamp) return arr[l][1];
+        return (l-1 >= 0) ? arr[l-1][1] : "" 
+    }
+}
