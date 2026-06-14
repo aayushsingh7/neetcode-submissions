df033e2b732cd32ac7d60324c477985c442b2f1c
Aayush Singh <97450185+aayushsingh7@users.noreply.github.com>
2026-06-14 13:32:30 +0530

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let set= new Set(), ans = 0;
+        let l = 0;
+        for(let r = 0; r < s.length; r++) {
+            while(set.has(s[r])) {
+                set.delete(s[l++])
+            }
+            set.add(s[r]);
+            ans = Math.max(ans, set.size);
+        }
+        return ans;
+    }
+}
